39974cc3fc1b6bbad4a06ee7eb1b3493d57ee4ca
ivan-mukhin <imukhin16@gmail.com>
2024-09-14 21:27:18 -0400

    - Changed name of Documentation to Notes to reduce confusion - Made a jsonParser function to open a JSON API response and parse it for easier access and use

---
+++ jsonParser.py
@@ -0,0 +1,18 @@
+import json
+
+def jsonParser(filePath):
+    try: 
+        file = open(filePath, 'r')
+        data = json.load(file)
+        return data
+
+    except FileNotFoundError:
+        print('Error: the file was not found')
+
+
+filePath = 'API_Resp_Connected_Realms'
+parsedData = jsonParser(filePath)
+connectedRealms = parsedData['realms']
+
+for realm in connectedRealms:
+    print(realm['name'], realm['id'] )
